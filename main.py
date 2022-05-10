5358cc82245583705c128fe34c9ad0cf34cc8cbc
Weijian Yang <424001967@qq.com>
2022-05-10 15:09:24 +0200

    Main function V3
    V3: cooperate with another file(normalization.py), use Tensorboard to monitor losses
    V2: add codes for saving results and exploration
    V1: the main function

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ Main function
 
 CUDA version: 11.2
 """
-import os
 from functions import *
+from normalization import *
 from maddpg import MADDPG
 from buffer import MultiAgentReplayBuffer
 from make_env import MultiAgentEnv
 from check_state import CheckState
+from torch.utils.tensorboard import SummaryWriter
 
 if __name__ == '__main__':
-    scenario = '2Ships_Cross'
+    scenario = '1Ship'
+    # scenario = '2Ships_Cross'
+    # scenario = '2Ships_Headon'
     # scenario = '3Ships_Cross&Headon'
     env = MultiAgentEnv(scenario)
     n_agents = env.ships_num
@@ -22,7 +25,8 @@ if __name__ == '__main__':
     n_actions = env.ship_action_space
 
     chkpt_dir = os.path.dirname(os.path.realpath(__file__)) + '\SavedNetwork'
-    maddpg_agents = MADDPG(chkpt_dir, actor_dims, critic_dims, n_agents, n_actions, alpha=0.01, beta=0.01)
+    maddpg_agents = MADDPG(chkpt_dir, actor_dims, critic_dims, n_agents, n_actions,
+                           fc1=env.ships_num * 32, fc2=16, alpha=0.01, beta=0.001)
 
     max_size = 1000000
     memory = MultiAgentReplayBuffer(max_size, actor_dims, critic_dims, n_agents, n_actions, batch_size=1024)
@@ -33,9 +37,11 @@ if __name__ == '__main__':
                            dis_redun, dis_safe)
 
     PRINT_INTERVAL = 500
-    N_GAMES = 30000
-    steps_max = 1000  # can be considered as the simulation time
-    steps_exp = N_GAMES / 2
+    steps_games = 20000
+    steps_exp = steps_games / 2
+    # a reasonable simulation time
+    steps_max = (((env.ships_dis_max / env.ships_vel_min)//500) + 1) * 500
+    print('... the maximum simulation step in each episode:', steps_max[0], '...')
     steps_total = 0
 
     evaluate = False
@@ -50,7 +56,11 @@ if __name__ == '__main__':
     result_dir = os.path.dirname(os.path.realpath(__file__)) + '\SavedResult'
     rewards_global = []
 
-    for i in range(N_GAMES + 1):
+    writer = SummaryWriter("SavedLosses")
+    writer_dir = os.path.dirname(os.path.realpath(__file__)) + '\SavedLosses'
+    delete_files(writer_dir)
+
+    for i in range(steps_games + 1):
         obs = env.reset()
         # limits on ships' heading angles
         obs[:, 2] = warp_to_360(obs[:, 2], env.ships_num)
@@ -68,10 +78,14 @@ if __name__ == '__main__':
         path_local.append(obs.reshape(1, -1))
         rewards_local =[]
 
+        reward_alive = 1
         while not done_reset:
             actions = maddpg_agents.choose_action(obs, Exploration)
-            # list type, example: [-1.0, -1.0]
+            # print(actions)
+
+            # list type, example: [-1.0, 1.0]
             obs_ = env.step(actions).copy()
+            # print(obs_)
 
             # For local observation problems, observations are not equal to states.
             # For global observation problems, observations are equal to states.
@@ -81,27 +95,61 @@ if __name__ == '__main__':
 
             # reward
             reward_term, done_goal = check_env.check_term(obs, obs_, done_goal)
+            reward_term_max = 20
             env.ships_done_goal = done_goal
 
-            reward_coll, done_coll = check_env.check_coll(obs, obs_)
+            # reward_coll, done_coll = check_env.check_coll(obs, obs_)
 
-            reward_CORLEG = check_env.check_CORLEGs(obs, obs_)
+            # reward_CORLEG = check_env.check_CORLEGs(obs, obs_)
 
-            reward = reward_term + reward_coll + reward_CORLEG
+            reward = reward_term - reward_alive
+            reward_max = reward_term_max - reward_alive
+            # reward = reward_term + reward_coll + reward_CORLEG - reward_alive
             # print(reward, reward_term, reward_coll, reward_CORLEG)
             rewards_local.append(reward)
 
             if step_episode >= steps_max:
                 done_reset = True
             # if all(done_goal):
+            # if i == 0:
+            #     print(i, state, done_goal)
             if any(done_goal):
                 done_reset = True
-            if done_coll:
-                done_reset = True
-            memory.store_transition(obs, state, actions, reward, obs_, state_, done_goal)
+            # if done_coll:
+            #     done_reset = True
 
-            if steps_total % 100 == 0 and not evaluate:
-                maddpg_agents.learn(memory)
+            # data normalization
+            n_obs = obs.copy()
+            n_reward = reward.copy()
+            n_obs_ = obs_.copy()
+            n_state_ = state_.copy()
+            for ship_idx in range(env.ships_num):
+                n_obs[ship_idx,  0] = \
+                    nmlz_pos(obs[ship_idx, 0], env.ships_x_min[ship_idx], env.ships_x_max[ship_idx])
+                n_obs[ship_idx, 1] = \
+                    nmlz_pos(obs[ship_idx, 1], env.ships_y_min[ship_idx], env.ships_y_max[ship_idx])
+                n_obs[ship_idx, 2] = nmlz_ang(obs[ship_idx, 2])
+
+                n_obs_[ship_idx, 0] = \
+                    nmlz_pos(obs_[ship_idx, 0], env.ships_x_min[ship_idx], env.ships_x_max[ship_idx])
+                n_obs_[ship_idx, 1] = \
+                    nmlz_pos(obs_[ship_idx, 1], env.ships_y_min[ship_idx], env.ships_y_max[ship_idx])
+                n_obs_[ship_idx, 2] = nmlz_ang(obs_[ship_idx, 2])
+
+                n_reward[ship_idx] = nmlz_r(reward[ship_idx], reward_max)
+            n_state = n_obs.reshape(1, -1)
+            n_state_ = n_obs_.reshape(1, -1)
+
+            memory.store_transition(n_obs, n_state, actions, n_reward, n_obs_, n_state_, done_goal)
+            if not memory.ready():
+                continue
+            elif steps_total % 100 == 0 and not evaluate:
+                critic_losses, actor_losses = maddpg_agents.learn(memory)
+                # Type in terminal: tensorboard --logdir=SavedLosses
+                # See losses in web page
+                for agent_idx in range(env.ships_num):
+                    writer.add_scalar('agent_%s' % agent_idx+'_critic_loss', critic_losses[agent_idx], steps_total)
+                    writer.add_scalar('agent_%s' % agent_idx + '_actor_loss', actor_losses[agent_idx], steps_total)
 
             obs = obs_.copy()
             path_local.append(state)
@@ -113,11 +161,12 @@ if __name__ == '__main__':
         if i == 0:
             score_best = score
             np.save(result_dir + '/path_test.npy', path_local)
-            np.save(result_dir + '/reward_test.npy', rewards_global)
+            np.save(result_dir + '/rewards_test.npy', rewards_global)
         elif score > score_best:
             score_best = score
             path_global = path_local
             rewards_global = rewards_local
+            print('... updating the optimal path group ( episode:', i, ', reward:', score_best, ') ...')
 
         score_history.append(score)
         score_avg = np.mean(score_history[-100:])
@@ -128,9 +177,11 @@ if __name__ == '__main__':
         if i % PRINT_INTERVAL == 0 and i > 0:
             print('episode', i, 'average score {:.1f}'.format(score_avg))
 
+    writer.close()
     # save networks
     maddpg_agents.save_checkpoint()
     # save data
+    np.save(result_dir + '/path_last.npy', path_local)
     np.save(result_dir + '/score_history.npy', score_history)
     np.save(result_dir + '/path_global.npy', path_global)
     np.save(result_dir + '/rewards_global.npy', rewards_global)
